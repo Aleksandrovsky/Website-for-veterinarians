77b3bfba9cb7ccefdcb37f4a5890470fae3a5234
Aleksandrovsky <olek@localhost.localdomain>
2023-03-13 17:48:49 +0000

    Developing 'auth.py' code

--- auth/auth.py
+++ auth/auth.py
@@ -1,16 +1,20 @@
-from flask import Flask, Blueprint, render_template, url_for, redirect
+from flask import Flask, Blueprint, render_template, url_for, redirect, session
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms import ValidationError
-from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo, IntegerField
+from wtforms import ValidationError, IntegerField
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
-import sqlite3
+from db.db import add_data
 
 import os
 
-from db.db import add_data
+import random
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 
@@ -20,7 +24,13 @@ bp_auth = Blueprint('bp_auth_form', __name__, template_folder='templates')
 
 db_path = (os.getcwd() + '/db_folder/database.db')
 
-generated_code = None
+table_name = 'users'
+
+smtp_adress = 'smtp.poczta.onet.pl'
+port = 465
+login = '#'
+password = '#'
+
 
 
 #ROUTES
@@ -32,7 +42,7 @@ def login():
 
 
 
-@bp_auth.route('/registration', methods=['GET', 'POST'])
+@bp_auth.route('/registration', methods=["GET", "POST"])
 def registration():
     
     form = Registration()
@@ -40,7 +50,6 @@ def registration():
     if form.validate_on_submit():
         
         data_dict = {}
-        table_name = 'users'
         
         email = form.email.data
         username = form.username.data
@@ -53,9 +62,8 @@ def registration():
             data_dict['username'] = username
             data_dict['hash_password'] = hash_password
             data_dict['admin'] = 0
-            
-            add_data(data_dict, table_name, db_path)
-        
+
+            session['data_dict'] = data_dict
         else: 
             raise ValueError('Hash password not match with password')
         
@@ -63,9 +71,33 @@ def registration():
         """ TRZEBA ZROBIĆ AUTENTYKACJĘ MEJLEM """
         
         
-        return redirect(url_for('bp_auth_form.check_code'))   
+        return redirect(url_for('bp_auth_form.check_code', username=username))   
      
     return render_template('registration.html', form=form)
+
+
+
+@bp_auth.route('/registration/check_code/<username>', methods=["GET", "POST"])
+def check_code(username):
+    
+    form_check = CheckCode()
+    
+    data_dict = session['data_dict']
+    
+    recipent_email = data_dict['email']
+    
+    auth_code = generate_auth_code()
+    send_auth_code(auth_code, recipent_email)
+    
+    if form_check.validate_on_submit():
+        code = form_check.code.data
+        
+        if code == auth_code:
+            
+            add_data(data_dict, table_name, db_path)
+            return redirect(url_for('bp_auth_form.reg_ok'))
+
+    return render_template('check_code.html', form_check = form_check)
 
 
 
@@ -75,19 +107,50 @@ def reg_ok():
 
 
 
-@bp_auth.route('/registration/check_code')
-def check_code():
-    
-    form_check = CheckCode()
-    
-    if form_check.validate_on_submit():
-        code = form_check.code.data
-        
-        if code == generated_code:
-            return redirect(url_for('bp_auth_form.check_code'))
+#FUNCTIONS
 
-            
-    return render_template('check_code.html', form_check = form_check)
+def generate_auth_code():
+    
+    auth_code = []
+    
+    for i in range(6):
+
+        i = random.randint(0,9)
+        auth_code.append(i)
+    
+    auth_code = [''.join(str(i) for i in auth_code)][0]
+
+    return auth_code
+
+
+
+def send_auth_code(auth_code, recipent_email):
+    
+    message_to_recipent = "Twój kod aktywacyjny: " + str(auth_code)
+
+    message_to_send = MIMEMultipart('alternative')
+    message_to_send['Subject'] = 'Veterinarians- authentication code.'
+    message_to_send['From'] = 'kretexus@onet.pl'
+    message_to_send['To'] = recipent_email
+    message_to_send.attach(MIMEText(message_to_recipent))
+
+    smtp_object = smtplib.SMTP_SSL(smtp_adress, port)
+    code, msg = smtp_object.ehlo()
+
+    if code == 250:
+        login_code, login_msg = smtp_object.login(login, password)
+        print('Connection ok.')
+
+        if login_code == 235:
+            smtp_object.sendmail('kretexus@onet.pl', recipent_email, message_to_send.as_string())
+            smtp_object.quit()
+            print('Auth code send.')
+
+        else:
+            print('Didn`t send! Code: ', login_code, 'Message: ', login_msg)
+
+    else:
+        print('Didn`t connect! Code: ', code, 'Message: ', msg)
 
 
 
@@ -109,3 +172,7 @@ class CheckCode(FlaskForm):
     
     code = IntegerField('Wpisz kod, który wysłaliśmy na adres e-mail podany w formularzu', validators=[DataRequired(), Length(6)])
     submit =  SubmitField('Wyślij')
+    
+# if __name__ == '__main__':
+    
+#     print(generate_auth_code())
